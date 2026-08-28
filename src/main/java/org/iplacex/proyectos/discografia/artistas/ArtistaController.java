cb5fd7d8a9efdcc701d5072294fe06d63ff46ce5
marc7soto-web <marc7soto@gmail.com>
2026-08-28 07:22:45 -0300

    Creacion del controlador Artista

---
+++ src/main/java/org/iplacex/proyectos/discografia/artistas/ArtistaController.java
@@ -0,0 +1,88 @@
+package org.iplacex.proyectos.discografia.artistas;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+
+public class ArtistaController {
+
+    @Autowired
+    private IArtistaRepository artistaRepository;
+
+    /* Inserta un nuevo artista */
+    @PostMapping(value = "/artista", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Artista> HandleInsertArtistaRequest(@RequestBody Artista artista) {
+
+        Artista artistaGuardado = artistaRepository.save(artista);
+
+        return new ResponseEntity<>(artistaGuardado, HttpStatus.CREATED);
+    }
+
+    /* Obtiene todos los artistas */
+    @GetMapping(value = "/artistas", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Artista>> HandleGetArtistasRequest() {
+
+        return new ResponseEntity<>(artistaRepository.findAll(), HttpStatus.OK);
+    }
+
+    /* Obtiene un artista por id */
+    @GetMapping(value = "/artista/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Artista> HandleGetArtistaRequest(@PathVariable String id) {
+
+        Optional<Artista> artista = artistaRepository.findById(id);
+
+        if (artista.isPresent()) {
+            return new ResponseEntity<>(artista.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /* Actualiza un artista */
+    @PutMapping(value = "/artista/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Artista> HandleUpdateArtistaRequest(
+            @PathVariable String id,
+            @RequestBody Artista artista) {
+
+        if (!artistaRepository.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        artista._id = id;
+
+        Artista artistaActualizado = artistaRepository.save(artista);
+
+        return new ResponseEntity<>(artistaActualizado, HttpStatus.OK);
+    }
+
+    /* Elimina un artista */
+    @DeleteMapping("/artista/{id}")
+    public ResponseEntity<String> HandleDeleteArtistaRequest(@PathVariable String id) {
+
+        if (!artistaRepository.existsById(id)) {
+            return new ResponseEntity<>("Artista no encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        artistaRepository.deleteById(id);
+
+        return new ResponseEntity<>("Artista eliminado correctamente", HttpStatus.OK);
+    }
+
+}
--- src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java
+++ src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java
@@ -1,7 +1,8 @@
 package org.iplacex.proyectos.discografia.artistas;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/* Repositorio para la colección artistas */
 public interface IArtistaRepository extends MongoRepository<Artista, String> {
-    
-    
+
 }
